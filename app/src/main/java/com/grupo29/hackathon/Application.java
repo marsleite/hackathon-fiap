c4b6293da48eec24cacacfdd2387d60f875bb2fc
Marcelo <marcelo.leite@mercadolivre.com>
2024-03-11 23:51:59 -0300

    feat: config to clean arch

--- app/src/main/java/com/grupo29/hackathon/Application.java
+++ app/src/main/java/com/grupo29/hackathon/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HackathonGrupo29Application {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(HackathonGrupo29Application.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
