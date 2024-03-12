fef640afa8a05c176b5e599e38f437f9c74e12b1
Marcelo <marcelo.leite@mercadolivre.com>
2024-03-11 23:26:09 -0300

    feat: initial commit

---
+++ src/main/java/com/grupo29/hackathon/HackathonGrupo29Application.java
@@ -0,0 +1,13 @@
+package com.grupo29.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonGrupo29Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HackathonGrupo29Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/grupo29/hackathon/HackathonGrupo29ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo29.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonGrupo29ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
