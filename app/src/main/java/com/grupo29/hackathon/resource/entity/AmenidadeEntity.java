c02f7f24752921a55b1bf3c5aaeda0f51c185d7d
CaioLacerda88 <33129964+CaioLacerda88@users.noreply.github.com>
2024-03-15 20:37:05 -0300

    Merge pull request #6
    * CRUD Cliente ajustes Quarto
    
    * Commit classes criadas
    
    * Merge branch 'main' into feature/cliente-quarto-usecases
    
    * Mock de serviço de email criado

--- app/src/main/java/com/grupo29/hackathon/resource/entity/AmenidadeEntity.java
+++ app/src/main/java/com/grupo29/hackathon/resource/entity/AmenidadeEntity.java
@@ -1,7 +1,9 @@
 package com.grupo29.hackathon.resource.entity;
 
 import com.grupo29.hackathon.model.quartos.Amenidade;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class AmenidadeEntity {
   private Amenidade amenidade;
 }
